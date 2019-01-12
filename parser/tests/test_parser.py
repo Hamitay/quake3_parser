20a373bea51cf4d1a1f4a90d9f91ca1114cce13d
Henrique Amitay <henriqueamitay@gmail.com>
2019-01-11 22:04:30 -0200

    better expected values for our parser test

--- parser/tests/test_parser.py
+++ parser/tests/test_parser.py
@@ -1,22 +1,22 @@
 from .. import parser
-
-PLAYERS_0 = ["Mocinha", "Zeh", "Isgalamido", "Dono da Bola"]
-PLAYERS_1 = ["Zeh", "Isgalamido", "Assasinu Credi", "Dono da Bola"]
-PLAYERS_2 = ["Isgalamido", "Zeh" , "Assasinu Credi", "Dono da Bola"]
+import json
 
 class TestParser:
 
     def setup(self):
         self.game_log = parser.open_log_file("resources/test_games.log")
 
+        with open("resources/test_response.json") as response_json:
+            self.expected_response = json.load(response_json)
+
     def test_split_games(self):
         games = parser.split_games(self.game_log)
-        assert 3 == len(games)
+        assert len(self.expected_response) == len(games)
 
     def test_get_players(self):
         games = parser.split_games(self.game_log)
         players = [parser.get_players(game) for game in games]
 
-        assert PLAYERS_0.sort() == players[0].sort()
-        assert PLAYERS_1.sort() == players[1].sort()
-        assert PLAYERS_2.sort() == players[2].sort()
+        assert self.expected_response["game_1"]["players"].sort() == players[0].sort()
+        assert self.expected_response["game_2"]["players"].sort() == players[1].sort()
+        assert self.expected_response["game_3"]["players"].sort() == players[2].sort()
