5d32b28490fb497e0b43b001d8125350ad17ae6c
Henrique Amitay <henriqueamitay@gmail.com>
2019-01-13 21:49:40 -0200

    Fixed server import

--- api/constants.py
+++ api/constants.py
@@ -1,3 +1,7 @@
+#Environment variables
 GAME_PATH_CONFIG = "GAME_LOG_PATH"
 GAME_PATH_ENV = "GAME_LOG_PATH"
+CONTAINER_ENV = "DOCKER"
+
+#Default values
 DEFAULT_GAME_PATH = "games.log"
--- api/server.py
+++ api/server.py
@@ -1,6 +1,6 @@
 
 from flask import Flask
-from .constants import GAME_PATH_CONFIG, GAME_PATH_ENV, DEFAULT_GAME_PATH
+from .constants import *
 import os
 from .parser_api import parser_app
 
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     app.config[GAME_PATH_CONFIG] = os.environ.get(GAME_PATH_ENV, default=DEFAULT_GAME_PATH)
     app.register_blueprint(parser_app)
 
-    if os.environ.get("DOCKER", default=False):
+    if os.environ.get(CONTAINER_ENV, default=False):
         app.run("0.0.0.0")
     else:
         app.run()
