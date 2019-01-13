35c874bdef046e2eb7ff59a5c61710d39e75299d
Henrique Amitay <henriqueamitay@gmail.com>
2019-01-13 16:06:57 -0200

    Tested the api response

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,13 +1,28 @@
 from api import parser_api
 from api import exception_messages
+from ..constants import GAME_PATH_CONFIG
+import os
 import pytest
 import json
 
 class TestApiGetGames:
+    def setup(self):
+        abs_path = os.path.dirname(__file__)
+        self.game_log_path = abs_path + "/resources/test_games.log"
+
+        response_path = abs_path + "/resources/test_response.json"
+        with open(response_path) as response_json:
+            self.expected_response = json.load(response_json)
+        
+        #Sorting, since player order doesn't matter
+        for key in self.expected_response:
+            self.expected_response[key]["players"].sort()
 
     @pytest.fixture(scope="class")
     def get_request(self):
+        self.setup()
         app = parser_api.app.test_client()
+        parser_api.app.config[GAME_PATH_CONFIG] = self.game_log_path
         return app.get("/games")
 
     def test_get_games_status_code(self, get_request):
@@ -16,21 +31,40 @@ class TestApiGetGames:
     def test_get_games_content_type(self, get_request):
         assert "application/json" == get_request.content_type
 
+    def test_data(self, get_request):
+        assert self.expected_response == json.loads(get_request.data)
+
 class TestApiGetGameById:
+    def setup(self):
+        abs_path = os.path.dirname(__file__)
+        self.game_log_path = abs_path + "/resources/test_games.log"
+
+        response_path = abs_path + "/resources/test_response_id.json"
+        with open(response_path) as response_json:
+            self.expected_response = json.load(response_json)
+        
+        #Sorting, since player order doesn't matter
+        self.expected_response["players"].sort()
+
+    def create_test_app(self):
+        app = parser_api.app.test_client()
+        parser_api.app.config[GAME_PATH_CONFIG] = self.game_log_path
+        return app
 
     @pytest.fixture(scope="class")
     def get_request(self):
-        app = parser_api.app.test_client()
+        self.setup()
+        app = self.create_test_app()
         return app.get("/games/9")
 
     @pytest.fixture(scope="class")
     def get_invalid_request(self):
-        app = parser_api.app.test_client()
+        app = self.create_test_app()
         return app.get("/games/aa")
 
     @pytest.fixture(scope="class")
     def get_not_found_request(self):
-        app = parser_api.app.test_client()
+        app = self.create_test_app()
         return app.get("/games/999")
 
     def test_get_games_by_id_status_code(self, get_request):
@@ -38,6 +72,9 @@ class TestApiGetGameById:
 
     def test_get_games_by_id_content_type(self, get_request):
         assert "application/json" == get_request.content_type
+
+    def test_get_games_by_id_response(self, get_request):
+        assert self.expected_response == json.loads(get_request.data)
 
     def test_bad_request_status_code(self, get_invalid_request):
         assert 400 == get_invalid_request.status_code
